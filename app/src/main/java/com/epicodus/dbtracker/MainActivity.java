ebb066c162c7e0883094e23410235246c71b9460
Paul Guevarra <p.a.guevarra@gmail.com>
2017-10-23 22:53:17 -0700

    gather data from edit text view when button is clicked

--- app/src/main/java/com/epicodus/dbtracker/MainActivity.java
+++ app/src/main/java/com/epicodus/dbtracker/MainActivity.java
@@ -3,22 +3,28 @@ package com.epicodus.dbtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindGameButton;
+    private EditText mGameSearchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mGameSearchEditText = (EditText) findViewById(R.id.gameSearchEditText);
         mFindGameButton = (Button) findViewById(R.id.findGameButton);
         mFindGameButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                String gameSearch = mGameSearchEditText.getText().toString();
+                Log.d(TAG, gameSearch);
                 //do something
 //                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, GamesActivity.class);
