d1bde81e612fb825e41e52044bccf3258160114a
Paul Guevarra <p.a.guevarra@gmail.com>
2017-10-23 20:40:21 -0700

    add click listener to find game button

--- app/src/main/java/com/epicodus/dbtracker/MainActivity.java
+++ app/src/main/java/com/epicodus/dbtracker/MainActivity.java
@@ -2,12 +2,23 @@ package com.epicodus.dbtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindGameButton = (Button) findViewById(R.id.findGameButton);
+            mFindGameButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    //do something
+                }
+            });
     }
 }
