adecdcbb5e584573c03f6374ff591eb81608f53c
Paul Guevarra <p.a.guevarra@gmail.com>
2017-10-23 20:46:49 -0700

    add toast to test onClickListener

--- app/src/main/java/com/epicodus/dbtracker/MainActivity.java
+++ app/src/main/java/com/epicodus/dbtracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindGameButton;
@@ -14,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFindGameButton = (Button) findViewById(R.id.findGameButton);
-            mFindGameButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    //do something
-                }
-            });
+        mFindGameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
